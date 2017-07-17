84133025786de8875fd9b37a50080951030acda2
Jaroslav Týc <mail@jaroslavtyc.com>
2017-07-17 14:18:02 +0200

    Thief skill properties have link to duration and another concentration level

--- thief.php
+++ thief.php
@@ -4,6 +4,7 @@ function thief_properties_highlighted(string $text)
     $highlighted = '';
     foreach (preg_split("~[\r\n]+~", $text, -1, PREG_SPLIT_NO_EMPTY) as $row) {
         $row = format_2k6_plus($row);
+        $row = add_duration_link($row);
         $row = add_concentration_link($row);
         $row = format_master_bonus($row);
         preg_match('~^(?<property>[^:]+):(?<description>(?:[^+]*|.+\+\d.*))$~', $row, $matches);
@@ -23,9 +24,16 @@ function format_2k6_plus(string $text): string
     return preg_replace('~2k6\+\s+~', '2k6<span class="upper-index">+</span>', $text);
 }
 
+function add_duration_link(string $text): string
+{
+    return preg_replace('~(Trvání:) (.+)~', '$1 <a href="https://pph.drdplus.info/#tabulka_casu">$2</a>', $text);
+}
+
 function add_concentration_link(string $text): string
 {
-    return str_replace('plné soustředění', '<a href="https://pph.drdplus.info/#Plné soustředění">plné soustředění</a>', $text);
+    return preg_replace_callback('~(plné|volné soustředění)~', function (array $matches) {
+        return '<a href="https://pph.drdplus.info/#' . ucfirst($matches[1]) . '">' . $matches[1] . '</a>';
+    }, $text);
 }
 
 function format_master_bonus(string $text)
